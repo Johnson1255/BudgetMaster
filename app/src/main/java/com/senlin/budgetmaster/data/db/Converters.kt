fdf54598f4d0c6a195714ca933694071ada0e23b
Johnson1255 <johnsonc12yt@gmail.com>
2025-04-29 17:41:55 -0500

    fix: Arreglo del error 'DateTimeException.' que arrojaba la aplicacion

--- app/src/main/java/com/senlin/budgetmaster/data/db/Converters.kt
+++ app/src/main/java/com/senlin/budgetmaster/data/db/Converters.kt
@@ -3,12 +3,17 @@ package com.senlin.budgetmaster.data.db
 import androidx.room.TypeConverter
 import com.senlin.budgetmaster.data.model.TransactionType
 // Removed java.util.Date import
+import java.time.Instant // Import Instant
 import java.time.LocalDate // Import LocalDate
+import java.time.ZoneId // Import ZoneId
 
 class Converters {
     @TypeConverter
     fun fromEpochDay(value: Long?): LocalDate? {
-        return value?.let { LocalDate.ofEpochDay(it) }
+        // Convert milliseconds since epoch to LocalDate
+        return value?.let {
+            Instant.ofEpochMilli(it).atZone(ZoneId.systemDefault()).toLocalDate()
+        }
     }
 
     @TypeConverter
